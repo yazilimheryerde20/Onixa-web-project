020926a6c7763c9a0a43e1baf03895a0785531ec
erkinakdeniz <53817303+erkinakdeniz@users.noreply.github.com>
2020-08-02 19:43:36 +0300

    Product Details sayfası yapımı

--- Onixa/Onixa_Web/Controllers/HomeController.cs
+++ Onixa/Onixa_Web/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace Onixa_Web.Controllers
         {
             return View();
         }
+
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
     }
 }
